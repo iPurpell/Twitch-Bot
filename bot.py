12514b90036438b6b7a607659fc5351970513d1a
Purpell <ipurpell@gmail.com>
2024-05-11 11:20:14 +0100

    Tarot command

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import os, pickle, datetime, random
 
 from models.user import User
-
 from pathlib import Path
 from twitchio.ext import commands, routines
 
@@ -17,7 +16,7 @@ class Bot(commands.Bot):
 
     def __init__(self):
         # Initialise our Bot with our access token, prefix and a list of channels to join on boot...
-        super().__init__(token=TOKEN, prefix='!', initial_channels=CHANNELS)
+        super().__init__(token=TOKEN, prefix='!!', initial_channels=CHANNELS)
         userdata_path = Path(USERDATAPATH)
         userdata_path.touch()
         try:
@@ -79,18 +78,188 @@ class Bot(commands.Bot):
     async def tarot(self, ctx: commands.Context):
         
         reverse: bool = random.choice([True, False])
-        card: int = random.randint(1,22)
+        card: int = random.randint(0,21)
         
         await ctx.send('Shuffling cards...')
 
         match card:
 
+            case 0:
+                if reverse == True:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "The Fool" reversed! 
+                                   Keywords: reckless, careless, distracted, naive, foolish, gullible, stale, dull')
+                else:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "The Fool" upright! 
+                                   Keywords: beginnings, freedom, innocence, originality, adventure, idealism, spontaneity')
+            
             case 1:
                 if reverse == True:
-                    await ctx.send(f'{ctx.author.name} Your card of the day is "CARD" reversed! Keywords:')
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "The Magician" reversed! 
+                                   Keywords: manipulation, cunning, trickery, wasted talent, illusion, deception')
                 else:
-                    await ctx.send(f'{ctx.author.name} Your card of the day is "CARD" upright! Keywords:')
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "The Magician" upright! 
+                                   Keywords: willpower, desire, being resourceful,, skill, ability, concentration, manifestation')
+            
+            case 2:
+                if reverse == True:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "The High Priestess" reversed! 
+                                   Keywords: repressed intuition, hidden motives, superficiality, confusion, cognitive dissonance')
+                else:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "The High Priestess" upright! 
+                                   Keywords: unconscious, intuition, mystery, spirituality, higher power, inner voice')
+            
+            case 3:
+                if reverse == True:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "The Empress" reversed! 
+                                   Keywords: insecurity, overbearing, negligence, smothering, lack of growth, lack of progress')
+                else:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "The Empress" upright! 
+                                   Keywords: divine feminine, sensuality, fertility, nurturing, creativity, beauty, abundance, nature')
 
+            case 4:
+                if reverse == True:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "The Emperor" reversed! 
+                                   Keywords: tyrant, domineering, rigid, stubborn, lack of discipline, recklessness')
+                else:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "The Emperor" upright! 
+                                   Keywords: stability, structure, protection, authority, control, practicality, focus, discipline')
+            
+            case 5:
+                if reverse == True:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "The Hierophant" reversed! 
+                                   Keywords: rebellion, unconventionality, non-conformity, new methods, ignorance')
+                else:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "The Hierophant" upright! 
+                                   Keywords: tradition, social groups, conventionality, conformity, education, knowledge, beliefs')
+
+            case 6:
+                if reverse == True:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "The Lovers" reversed! 
+                                   Keywords: disharmony, imbalance, conflict, detachment, bad choices, indecision')
+                else:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "The Lovers" upright! 
+                                   Keywords: love, unions, partnerships, relationships, choices, romance, balance, unity')
+
+            case 7:
+                if reverse == True:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "The Chariot" reversed! 
+                                   Keywords: forceful, no direction, no control, powerless, aggression, obstacles')
+                else:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "The Chariot" upright! 
+                                   Keywords: success, ambition, determination, willpower, control, self-discipline, focus')
+
+            case 8:
+                if reverse == True:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "Strength" reversed! 
+                                   Keywords:  self-doubt, weakness, low confidence, inadequacy, cowardice, forcefulness')
+                else:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "Strength" upright! 
+                                   Keywords: courage, bravery, confidence, compassion, self-confidence, inner power')
+
+            case 9:
+                if reverse == True:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "The Hermit" reversed! 
+                                   Keywords: loneliness, isolation, recluse, being anti-social, rejection, returning to society')
+                else:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "The Hermit" upright! 
+                                   Keywords: self-reflection, introspection, contemplation, withdrawal, solitude, search for self')
+
+            case 10:
+                if reverse == True:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "The Wheel of Fortune" reversed! 
+                                   Keywords: bad luck, lack of control, clinging to control, unwelcome changes, delays')
+                else:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "The Wheel of Fortune" upright! 
+                                   Keywords: change, cycles, fate, decisive moments, luck, fortune, unexpected events')
+
+            case 11:
+                if reverse == True:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "Justice" reversed! 
+                                   Keywords: injustice, retribution, dishonesty, corruption, dishonesty, unfairness, avoiding accountability')
+                else:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "Justice" upright! 
+                                   Keywords: justice, karma, consequence, accountability, law, truth, honesty, integrity, cause and effect')
+
+            case 12:
+                if reverse == True:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "The Hanged Man" reversed! 
+                                   Keywords: stalling, disinterest, stagnation, avoiding sacrifice, standstill, apathy')
+                else:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "The Hanged Man" upright! 
+                                   Keywords: sacrifice, waiting, uncertainty, lack of direction, perspective, contemplation')
+
+            case 13:
+                if reverse == True:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "Death" reversed! 
+                                   Keywords: fear of change, repeating negative patterns, resisting change, stagnancy, decay')
+                else:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "Death" upright! 
+                                   Keywords: transformation, endings, change, transition, letting go, release')
+
+            case 14:
+                if reverse == True:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "Temperance" reversed! 
+                                   Keywords: imbalance, excess, extremes, discord, recklessness, hastiness')
+                else:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "Temperance" upright! 
+                                   Keywords: balance, peace, patience, moderation, calm, tranquillity, harmony, serenity')
+
+            case 15:
+                if reverse == True:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "The Devil" reversed! 
+                                   Keywords: independence, freedom, revelation, release, reclaiming power, reclaiming control')
+                else:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "The Devil" upright! 
+                                   Keywords: oppression, addiction, obsession, dependency, excess, powerlessness, limitations')
+
+            case 16:
+                if reverse == True:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "The Tower" reversed! 
+                                   Keywords: averting disaster, delaying the inevitable, resisting change')
+                else:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "The Tower" upright! 
+                                   Keywords: disaster, destruction, upheaval, trauma, sudden change, chaos')
+            
+            case 17:
+                if reverse == True:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "The Star" reversed! 
+                                   Keywords: hopelessness, despair, negativity, lack of faith, despondent')
+                else:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "The Star" upright! 
+                                   Keywords: hope, inspiration, positivity, faith, renewal, healing, rejuvenation')
+            
+            case 18:
+                if reverse == True:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "The Moon" reversed! 
+                                   Keywords: fear, deception, anxiety, misunderstanding, misinterpretation, clarity, understanding')
+                else:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "The Moon" upright! 
+                                   Keywords: illusion, intuition, uncertainty, confusion, complexity, secrets, unconscious')
+
+            case 19:
+                if reverse == True:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "The Sun" reversed! 
+                                   Keywords: blocked happiness, excessive enthusiasm, pessimism, unrealistic expectations, conceitedness')
+                else:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "The Sun" upright! 
+                                   Keywords: happiness, success, optimism, vitality, joy, confidence, happiness, truth')
+
+            case 20:
+                if reverse == True:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "Judgement" reversed! 
+                                   Keywords: self-doubt, lack of self-awareness, failure to learn lessons, self-loathing')
+                else:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "Judgement" upright! 
+                                   Keywords: self-evaluation, awakening, renewal, purpose, reflection, reckoning')
+
+            case 21:
+                if reverse == True:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "The World" reversed! 
+                                   Keywords: lack of closure, lack of achievement, feeling incomplete, emptiness')
+                else:
+                    await ctx.send(f'{ctx.author.name} Your card of the day is "The World" upright! 
+                                   Keywords: completion, achievement, fulfilment, sense of belonging, wholeness, harmony')
+            
             case _:
                 print("Something went wrong!")
 
